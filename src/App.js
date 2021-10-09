0ad2f50e25ab82f96d896f459f720b0c63a6202b
Marija Nikolova <Marija.Nikolova@netcetera.com>
2021-10-09 10:26:16 +0200

    Added prettier and initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Col />
+        <Row>
+          <Col>
+            {' '}
+            <Alert variant="primary">Welcome to the web savings calculator! Please login / sign up to continue !</Alert>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </div>
   );
 }
